05992da232e6d5633adb5c396de6e844fbcef552
RamblingCookieMonster <RamblingCookieMonster@users.noreply.github.com>
2016-09-07 08:35:09 -0400

    Fix psd1 module version... [skip ci]

--- .build/0.build.ps1
+++ .build/0.build.ps1
@@ -2,8 +2,9 @@
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 Install-Module PSDepend -Force
 
-Invoke-PSDepend -Force
+Invoke-PSDepend .\.build\1.depend.psd1 -Force
 Set-BuildEnvironment
+
 Invoke-psake .\.build\2.psake.ps1
 
 exit ( [int]( -not $psake.build_success ) )
