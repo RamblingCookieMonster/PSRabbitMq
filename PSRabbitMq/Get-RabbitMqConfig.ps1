d485ef832b7dacef46c1ec5cba4db3fa5a8a567c
Raphael Fäh <raphael.faeh@corporatesoftware.ch>
2017-06-27 07:30:51 +0200

    Added a Scope Parameter to Set-RabbitMQConfig, changed the Source Parameter of Get-RabbitMQConfig to reflect the same values (In addition to the "old" ones). Adjusted the tests.

--- PSRabbitMq/Get-RabbitMqConfig.ps1
+++ PSRabbitMq/Get-RabbitMqConfig.ps1
@@ -10,23 +10,26 @@
         Config source:
         RabbitMqConfig to view module variable
         PSRabbitMq.xml to view PSRabbitMq.xml from "$env:APPDATA\PSRabbitMq.xml"
+        CurrentUser to view PSRabbitMq.xml from "$env:APPDATA\PSRabbitMq.xml"
+        System to view PSRabbitMq.xml from "$env:PROGRAMDATA\PSRabbitMq.xml"
 
     .FUNCTIONALITY
         RabbitMq
     #>
     [cmdletbinding()]
     param(
-        [ValidateSet('RabbitMqConfig','PSRabbitMq.xml')]
+        [ValidateSet('RabbitMqConfig','PSRabbitMq.xml','CurrentUser','System')]
         [string]$Source = "RabbitMqConfig"
     )
     
-    if($Source -eq "RabbitMqConfig")
-    {
+    if($Source -eq "RabbitMqConfig"){
         $Script:RabbitMqConfig
     }
-    else
-    {
+    elseif($Source -eq "PSRabbitMq.xml" -or $Source -eq "CurrentUser"){
         Import-Clixml -Path "$env:APPDATA\PSRabbitMq.xml"
+    }
+    elseif($Source -eq "System"){
+        Import-Clixml -Path "$env:PROGRAMDATA\PSRabbitMq.xml"
     }
 
 }
